c20f2fb5a1dda892f1b7b77a1e0e1a71942d5fcc
lukezdz <lukas.zdziarski@gmail.com>
2020-06-13 16:24:17 +0200

    Clean up dataset and retrain NN

--- ModelTraining/Trainer.py
+++ ModelTraining/Trainer.py
@@ -2,7 +2,9 @@ import pickle
 import os
 import pandas as pd
 import numpy as np
+import keras
 import tensorflow as tf
+import keras.backend.tensorflow_backend as tfback
 from DataLoader import DataLoader
 
 # backend.set_image_dim_ordering('th')
@@ -12,7 +14,7 @@ class Trainer:
 	_TESTDATA_FILENAME = 'testData.csv'
 
 	def __init__(self, epochs=100, batch_size=200, shuffle=True, verbose=1):
-		# tfback._get_available_gpus = self._get_available_gpus
+		tfback._get_available_gpus = self._get_available_gpus
 		self.epochs = epochs
 		self.batch_size = batch_size
 		self.shuffle = shuffle
@@ -28,13 +30,13 @@ class Trainer:
 		self.train_labels = self.train_data[['784']]
 		self.train_data.drop(self.train_data.columns[[784]], axis = 1, inplace = True)
 		self.train_labels = np.array(self.train_labels)
-		self.train_categories = tf.keras.utils.to_categorical(self.train_labels)
+		self.train_categories = keras.utils.to_categorical(self.train_labels)
 		
 		self.test_data = dataLoader.get_test_data(self._TESTDATA_FILENAME)
 		self.test_labels = self.test_data[['784']]
 		self.test_data.drop(self.test_data.columns[[784]], axis = 1, inplace = True)
 		self.test_labels = np.array(self.test_labels)
-		self.test_categories = tf.keras.utils.to_categorical(self.test_labels)
+		self.test_categories = keras.utils.to_categorical(self.test_labels)
 
 	# fixes a bug in keras and tf 2.1
 	def _get_available_gpus(self):
@@ -44,23 +46,24 @@ class Trainer:
 			A list of available GPU devices.
 		"""
 		#global _LOCAL_DEVICES
-		if tf.keras.backend._LOCAL_DEVICES is None:
+		if tfback._LOCAL_DEVICES is None:
 			devices = tf.config.list_logical_devices()
 			tfback._LOCAL_DEVICES = [x.name for x in devices]
-		return [x for x in tf.keras.backend._LOCAL_DEVICES if 'device:gpu' in x.lower()]
+		return [x for x in tfback._LOCAL_DEVICES if 'device:gpu' in x.lower()]
 
 	def _initialize_model(self):
-		self.model = tf.keras.models.Sequential()
-		self.model.add(tf.keras.layers.Conv2D(30, (3, 3), activation='relu', input_shape=(1,28,28), data_format='channels_first'))
-		self.model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
-		self.model.add(tf.keras.layers.Conv2D(15, (3,3), activation='relu'))
-		self.model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
-		self.model.add(tf.keras.layers.Dropout(0.2))
-		self.model.add(tf.keras.layers.Flatten())
-		self.model.add(tf.keras.layers.Dense(128, activation='relu'))
-		self.model.add(tf.keras.layers.Dense(80, activation='relu'))
-		self.model.add(tf.keras.layers.Dense(80, activation='relu'))
-		self.model.add(tf.keras.layers.Dense(22, activation='softmax'))
+		self.model = keras.models.Sequential()
+		# self.model.add(keras.layers.InputLayer(input_shape=(1, 28, 28)))
+		self.model.add(keras.layers.Conv2D(30, (3, 3), activation='relu', data_format='channels_first', input_shape=(1, 28, 28)))
+		self.model.add(keras.layers.MaxPooling2D(pool_size=(2, 2)))
+		self.model.add(keras.layers.Conv2D(15, (3,3), activation='relu'))
+		self.model.add(keras.layers.MaxPooling2D(pool_size=(2, 2)))
+		self.model.add(keras.layers.Dropout(0.2))
+		self.model.add(keras.layers.Flatten())
+		self.model.add(keras.layers.Dense(128, activation='relu'))
+		self.model.add(keras.layers.Dense(80, activation='relu'))
+		self.model.add(keras.layers.Dense(80, activation='relu'))
+		self.model.add(keras.layers.Dense(21, activation='softmax'))
 
 		self.model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
@@ -79,11 +82,11 @@ class Trainer:
 			verbose=self.verbose,
 			validation_split=0.02,
 			callbacks=[
-				tf.keras.callbacks.EarlyStopping(patience=10),
-				tf.keras.callbacks.ModelCheckpoint('model.hdf5', save_best_only=True)
+				keras.callbacks.EarlyStopping(patience=10),
+				keras.callbacks.ModelCheckpoint('model.hdf5', save_best_only=True)
 			]
 		)
-		self.model = tf.keras.models.load_model('model.hdf5')
+		#self.model = keras.models.load_model('model.hdf5')
 
 	def evaluate(self):
 		l = []
@@ -103,8 +106,11 @@ class Trainer:
 		print("Loss value: " + str(loss_value) + "; Accuracy: " + str(accuracy_value))
 		return accuracy_value
 
-	def save_model(self, filename):
+	def save_model(self):
 		model_json = self.model.to_json()
-		with open(filename + ".json", "w") as json_file:
+		with open("model.json", "w") as json_file:
 			json_file.write(model_json)
-		tf.keras.models.save_model(self.model, "model.hdf5")
+		self.model.save_weights("weights.h5")
+
+	def save_model_AIO(self):
+		keras.models.save_model(self.model, "saved_model.hdf5")
--- SmartCalcDesktop/Engine.py
+++ SmartCalcDesktop/Engine.py
@@ -10,7 +10,7 @@ class Engine:
 		tfback._get_available_gpus = self._get_available_gpus
 		self.testing = False
 		self.model: keras.models.Sequential = keras.models.load_model("model.hdf5")
-		self.character_map = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "+", "-", "*", "/", "=", "(", ")"]
+		self.character_map = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "+", "-", "*", "/", "(", ")"]
 
 	def _get_available_gpus(self):
 		"""Get a list of available gpu devices (formatted as strings).
@@ -30,107 +30,74 @@ class Engine:
 		print(equation)
 
 	def get_equation(self):
-		img = cv2.imread('testtest.png', cv2.IMREAD_GRAYSCALE)
+		img = cv2.imread('test_images/simple.jpg', cv2.IMREAD_GRAYSCALE)
 		cv2.imshow('wo', img)
 		cv2.waitKey()
 		cv2.destroyAllWindows()
 
-		if img is not None:
-			img = ~img
-			ret, thresh = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
-			ctrs, ret = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-			cnt = sorted(ctrs, key=lambda ctr: cv2.boundingRect(ctr)[0])
-			w = 28
-			h = 28
-			train_data = []
-			rects = []
-			for c in cnt:
-				x,y,w,h = cv2.boundingRect(c)
-				rect = [x,y,w,h]
-				rects.append(rect)
-			bool_rect = []
-			for r in rects:
-				l = []
-				for rec in rects:
-					flag=0
-					if rec!=r:
-						if r[0]<(rec[0]+rec[2]+10) and rec[0]<(r[0]+r[2]+10) and r[1]<(rec[1]+rec[3]+10) and rec[1]<(r[1]+r[3]+10):
-							flag=1
-						l.append(flag)
-					if rec == r:
-						l.append(0)
-				bool_rect.append(l)
-			dump_rect = []
-			for i in range(0, len(cnt)):
-				for j in range(0, len(cnt)):
-					if bool_rect[i][j] == 1:
-						area1 = rects[i][2]*rects[i][3]
-						area2 = rects[j][2]*rects[j][3]
-						if (area1 == min(area1, area2)):
-							dump_rect.append(rects[i])
-			final_rect = [i for i in rects if i not in dump_rect]
+		ans = ""
+		data = self._cut_image(img)
+		if data is None:
+			return ans
 
-			for r in final_rect:
-				x=r[0]
-				y=r[1]
-				w=r[2]
-				h=r[3]
-				im_crop = thresh[y:y+h+10, x:x+w+10]
+		for i in range(len(data)):
+			data[i] = np.array(data[i])
+			data[i] = data[i].reshape(1,1,28,28)
+			result = self.model.predict_classes(data[i])
+			ans += self.character_map[result[0]]
+		print(ans)
 
-				im_resize = cv2.resize(im_crop, (28, 28))
-				cv2.imshow('work', im_resize)
-				cv2.waitKey()
-				cv2.destroyAllWindows()
+	def _cut_image(self, img):
+		if img is None:
+			return None
 
-				im_resize = np.reshape(im_resize, (1,28,28))
-				train_data.append(im_resize)
+		img = ~img
+		ret, thresh = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+		ctrs, ret = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+		cnt = sorted(ctrs, key=lambda ctr: cv2.boundingRect(ctr)[0])
+		w = 28
+		h = 28
+		result = []
+		rects = []
+		for c in cnt:
+			x,y,w,h = cv2.boundingRect(c)
+			rect = [x,y,w,h]
+			rects.append(rect)
+		bool_rect = []
+		for r in rects:
+			l = []
+			for rec in rects:
+				flag=0
+				if rec!=r:
+					if r[0]<(rec[0]+rec[2]+10) and rec[0]<(r[0]+r[2]+10) and r[1]<(rec[1]+rec[3]+10) and rec[1]<(r[1]+r[3]+10):
+						flag=1
+					l.append(flag)
+				if rec == r:
+					l.append(0)
+			bool_rect.append(l)
+		dump_rect = []
+		for i in range(0, len(cnt)):
+			for j in range(0, len(cnt)):
+				if bool_rect[i][j] == 1:
+					area1 = rects[i][2]*rects[i][3]
+					area2 = rects[j][2]*rects[j][3]
+					if (area1 == min(area1, area2)):
+						dump_rect.append(rects[i])
+		final_rect = [i for i in rects if i not in dump_rect]
 
-			ans = ""
-			for i in range(len(train_data)):
-				train_data[i] = np.array(train_data[i])
-				train_data[i] = train_data[i].reshape(1,1,28,28)
-				result = self.model.predict_classes(train_data[i])
-				ans += self.character_map[result[0]]
-			print(ans)
+		for r in final_rect:
+			x=r[0]
+			y=r[1]
+			w=r[2]
+			h=r[3]
+			im_crop = thresh[y:y+h+10, x:x+w+10]
 
+			im_resize = cv2.resize(im_crop, (28, 28))
+			cv2.imshow('work', im_resize)
+			cv2.waitKey()
+			cv2.destroyAllWindows()
 
-	# def get_equation(self, image) -> str:
-	# 	image = ~image
-	# 	if image is None:
-	# 		return "error"
+			im_resize = np.reshape(im_resize, (1,28,28))
+			result.append(im_resize)
 		
-	# 	ret, thresholded = cv2.threshold(image, 127, 255, cv2.THRESH_BINARY)
-	# 	contours, ret = cv2.findContours(thresholded, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-	# 	#count = sorted(contours, key = lambda contour: cv2.boundingRect(contour)[0])
-	# 	rects = []
-	# 	for c in contours:
-	# 		approximated = cv2.approxPolyDP(c, 3, False)
-	# 		rects.append(cv2.boundingRect(c))
-	# 	width = 28
-	# 	height = 28
-	# 	maxi = 0
-
-	# 	selected = []
-	# 	for r in rects:
-	# 		# x, y, width, height = cv2.boundingRect(r)
-	# 		x, y, width, height = r
-	# 		maxi = max(width*height, maxi)
-	# 		if maxi == width * height:
-	# 			x_max = x
-	# 			y_max = y
-	# 			width_max = width
-	# 			height_max = height
-	# 		im_crop = thresholded[y_max : y_max + height_max + 10, x_max : x_max + width_max + 10]
-	# 		im_resize = cv2.resize(im_crop, (28, 28))	
-	# 		im_resize = np.reshape(im_resize, (784, 1))
-	# 		im_resize = np.reshape(im_resize, (1, 28, 28))
-	# 		selected.append(im_resize)
-		
-	# 	ans = ""
-	# 	for s in selected:
-	# 		ans += self.get_character(s)
-	# 	return ans
-	
-	# def get_character(self, pixels_array) -> str:
-	# 	ans = self.model.predict_classes(pixels_array)
-	# 	return self.character_map[ans]
+		return result			
