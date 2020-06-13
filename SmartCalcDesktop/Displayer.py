6f353b66b0e86eabb253d66aeed5daca8cf5082c
creativecbr <lesniakcbr@gmail.com>
2020-06-13 15:43:06 +0200

    Displayer class added

---
+++ SmartCalcDesktop/Displayer.py
@@ -0,0 +1,9 @@
+class Displayer:
+    equation = ""
+
+    def __init(self, _stringEquation):
+        self.equation = _stringEquation
+        self.Calculate()
+
+    def Calculate(selfs):
+        print("a dźwigu")
